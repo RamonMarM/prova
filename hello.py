65202b5bb8f9a3f6854976b872eba8d3acf4ea80
RamonMarM <120236024+RamonMarM@users.noreply.github.com>
2024-08-30 21:51:09 -0300

    comit2

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -7,14 +8,12 @@ from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from datetime import datetime
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 bootstrap = Bootstrap(app)
@@ -22,7 +21,8 @@ moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-DISCS = [('DSWA5', 'DSWA5'), ('GPSA5', 'GPSA5'), ('IHCA5', 'IHCA5'), ('SODA5', 'SODA5'), ('PJIA5', 'PJIA5'), ('TCOA5', 'TCOA5') ]
+# Choices for the select field in the form
+DISC = [('DSWA5', 'DSWA5'), ('GPSA5', 'GPSA5'), ('IHCA5', 'IHCA5'), ('SODA5', 'SODA5'), ('PJIA5', 'PJIA5'), ('TCOA5', 'TCOA5')]
 
 class Disc(db.Model):
     __tablename__ = 'discs'
@@ -44,33 +44,27 @@ class User(db.Model):
 
 class NameForm(FlaskForm):
     name = StringField('Cadastre o novo Professor', validators=[DataRequired()])
-    disc = SelectField('Disciplina associada?', choices=DISCS, validators=[DataRequired()])
+    disc = SelectField('Disciplina associada?', choices=DISC, validators=[DataRequired()])
     submit = SubmitField('Cadastrar')
-
-@app.shell_context_processor
-def make_shell_context():
-    return dict(db=db, User=User, Disc=Disc)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-
 @app.route('/')
 def index():
-     return render_template('index.html', current_time=datetime.utcnow())
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/professores', methods=['GET', 'POST'])
 def professores():
     form = NameForm()
     user_all = User.query.all()
     discs = Disc.query.all()
-    
+
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
@@ -88,10 +82,9 @@ def professores():
         session['name'] = form.name.data
         session['disc'] = form.disc.data
         return redirect(url_for('professores'))
+    
     return render_template('professores.html', form=form, name=session.get('name'),
-                           known=session.get('known', False),
-                           user_all=user_all, discs=discs)
-
+                           known=session.get('known', False), user_all=user_all, discs=discs)
 
 if __name__ == '__main__':
     app.run(debug=True)
